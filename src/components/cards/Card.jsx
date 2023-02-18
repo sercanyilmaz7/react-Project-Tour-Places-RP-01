49205d1e73c3362192458a6846f27dace00ef656
sercanyilmaz7 <srcn.yilmaz333@gmail.com>
2023-02-18 17:29:13 +0300

    first commit

---
+++ src/components/cards/Card.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Card.css";
+import { data } from "../../helper/data";
+
+const Card = () => {
+  console.log(data);
+  return (
+    <div className="card-container">
+      {data.map((item,index) => {
+        const{id,title,desc,image}=item
+        return (
+          <div className="cards" key={index}>
+            <h3 className="title">{title}</h3>
+            <img className="image" src={image} alt={title} />
+            <p className="text">{desc}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>POPULAR TOUR PLACES</div>
+  )
+}
+
+export default Header
